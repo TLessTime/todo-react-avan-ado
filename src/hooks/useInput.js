a7b83a783360e3aa83d054ab023d5d1054f66e14
TLessTime <felpeko777@gmail.com>
2025-09-18 10:25:45 -0300

    Create useInput.js

---
+++ src/hooks/useInput.js
@@ -0,0 +1,11 @@
+```js
+import { useCallback, useState } from 'react'
+
+
+export default function useInput(initial = '') {
+const [value, setValue] = useState(initial)
+const onChange = useCallback(e => setValue(e.target.value), [])
+const reset = useCallback(() => setValue(''), [])
+return { value, setValue, onChange, reset }
+}
+```
