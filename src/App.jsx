76732a27b878c72d35a30a3a99511dbf3d460d5f
TLessTime <felpeko777@gmail.com>
2025-09-18 10:23:54 -0300

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+```jsx
+import React from 'react'
+import TodoForm from './components/TodoForm'
+import TodoFilters from './components/TodoFilters'
+import TodoList from './components/TodoList'
+
+
+export default function App() {
+return (
+<div className="app">
+<header>
+<h1>Todo React Avançado</h1>
+<p>Hooks, Context, Hooks customizados e memoization</p>
+</header>
+
+
+<main>
+<TodoForm />
+<TodoFilters />
+<TodoList />
+</main>
+
+
+<footer>
+<small>Feito com ♥ usando React</small>
+</footer>
+</div>
+)
+}
+```
