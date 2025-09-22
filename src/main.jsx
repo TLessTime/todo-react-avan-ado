12b8a37887fd18ac5b91c4c508d1f822dd53f677
TLessTime <felpeko777@gmail.com>
2025-09-22 11:28:43 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-```jsx
+jsx
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
@@ -13,4 +13,4 @@ createRoot(document.getElementById('root')).render(
 </TodoProvider>
 </React.StrictMode>
 )
-```
+
